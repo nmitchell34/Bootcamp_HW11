5c4badfc405b0f6864732b77a1bee95b98bef9fa
NCM <nmitchell34@gatech.edu>
2020-07-06 20:42:44 -0400

    redid files, added server.js

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+// Data processing for POST routes
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// View/HTML Routes
+
+
+
+// API/JSON Routes
+app.get("/",(req,res)=>{
+    res.send("Hello World!")
+})
+
+
+//Listening to PORT
+app.listen(PORT, (req,res)=>{
+    console.log(`Currently Running on http://localhost:${PORT}`)
+})
+
